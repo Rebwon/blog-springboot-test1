b746f66a8756fd7a9b69959a441c7288a0040bb6
rebwon <msolo021015@gmail.com>
2018-10-21 21:49:45 +0900

    Initial commit

---
+++ src/main/java/ko/maeng/demospringtest/DemospringtestApplication.java
@@ -0,0 +1,12 @@
+package ko.maeng.demospringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemospringtestApplication.class, args);
+    }
+}
